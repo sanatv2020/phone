cca5fe6026ccf83eab4459502d2fc623b3500c1e
sanatv2020 <62134129+sanatv2020@users.noreply.github.com>
2020-09-02 14:47:26 +0000

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDbLouOhg5f2tqdtJ34HyJ7T1ZMlQYwSTQ",
+    authDomain: "phoneauthenticationreact-dd0b7.firebaseapp.com",
+    databaseURL: "https://phoneauthenticationreact-dd0b7.firebaseio.com",
+    projectId: "phoneauthenticationreact-dd0b7",
+    storageBucket: "phoneauthenticationreact-dd0b7.appspot.com",
+    messagingSenderId: "466830886842",
+    appId: "1:466830886842:web:306e881bf3b61de1547f83"
+}
+
+export default config;
